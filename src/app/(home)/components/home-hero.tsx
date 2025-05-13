26333d68de9d662c163f68c37f9776e896338b6a
jvittor <102699222+jvittor@users.noreply.github.com>
2025-05-13 14:56:18 -0300

    :recycle: refactor: adiciona imagem de fundo na hero da home

--- src/app/(home)/components/home-hero.tsx
+++ src/app/(home)/components/home-hero.tsx
@@ -1,11 +1,10 @@
-const Hero = () => {
+export default function HomeHero() {
   return (
-    <div className="bg-white flex h-[100vh] w-full items-center justify-center">
-        <div className="p-5 w-full flex justify-center items-center">
-            <p className="text-black">hello world</p>
-        </div>
+    <div className="relative flex flex-col items-center justify-center pb-10 pt-10 sm:flex-row min-h-screen">
+      <div className="absolute inset-0 bg-white opacity-0 lg:bg-[url('/home/image-background.png')] lg:opacity-75" />
+      <div className="relative z-10 flex w-full items-center justify-center sm:h-full">
+        teste
+      </div>
     </div>
   );
-};
-
-export default Hero;
+}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,15 @@
 // import { CTASection } from './components/cta-section';
-import Hero from './components/home-hero';
+import HomeHero from './components/home-hero';
 
 const Home = () => {
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <Hero />
+      <HomeHero />
     </div>
   );
 };
 
+
+// Dados, Mapas, Documentos, Vuscan ->
+// analisar localização
 export default Home;
